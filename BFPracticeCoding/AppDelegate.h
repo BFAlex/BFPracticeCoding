24268c85d6128d2880899fa81518346030e1107e
BFAlex <bfalex@BFAlexdeMac.local>
2018-04-01 16:13:15 +0800

    Initial Commit

---
+++ BFPracticeCoding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFPracticeCoding
+//
+//  Created by BFAlex on 2018/4/1.
+//  Copyright © 2018年 BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFPracticeCoding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFPracticeCoding
+//
+//  Created by BFAlex on 2018/4/1.
+//  Copyright © 2018年 BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
